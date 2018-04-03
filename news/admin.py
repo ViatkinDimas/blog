b039c6aacd2c2d761a50e3f654d28ec6da2e426c
Grom <viatkindima@gmail.com>
2018-04-03 06:35:02 +0300

    Grom-2

---
+++ news/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from news.models import News, Category, Tag, Comments
+
+
+class NewsAdmin(SummernoteModelAdmin):
+    """ Новости
+    """
+    list_display = ("title", "user", "created")
+    list_editable = ("user", )
+    search_fields = ["title", "user__username"]
+    list_filter = ("user", "created")
+    summer_note_fields = ('text_min', 'text')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    """ Комментарии
+    """
+    list_display = ('user', 'new', 'created', 'moderation')
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Comments, CommentAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
+# Register your models here.
---
+++ news/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from news.models import News, Comments
+from news.forms import CommentForm
+from datetime import datetime, timedelta
+
+
+def news_list(request):
+    """Вывод всех новостей
+    """
+    news = News.objects.all()
+    return render(request, "news/news_list.html", {"news": news})
+
+
+def new_single(request, pk):
+    """Вывод полной статьи
+    """
+    new = get_object_or_404(News, id=pk)
+    comment = Comments.objects.filter(new=pk, moderation=True)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.user = request.user
+            form.new = new
+            form.save()
+            return redirect(new_single, pk)
+    else:
+        form = CommentForm()
+    return render(request, "news/new_single.html",
+                  {"new": new,
+                   "comments": comment,
+                   "form": form})
+
+
+def news_filter(request, pk):
+    """ Фильтр статей по дате
+    """
+    news = News.objects.all()
+    if pk == 1:
+        now = datetime.now() - timedelta(minutes=60 * 24 * 7)
+        news = news.filter(created__gte=now)
+    elif pk == 2:
+        now = datetime.now() - timedelta(minutes=60 * 24 * 30)
+        news = news.filter(created__gte=now)
+    elif pk == 3:
+        news = news
+
+    return render(request, "news/news_list.html", {"news": news})
+
+
+# Create your views here.
